078fe26e94d128743c41139c665235c1fa4fb07c
magicdoughnut <magicdoughnut@github.com>
2014-05-13 17:20:41 +0100

    uploaded Match.py

---
+++ Match.py
@@ -0,0 +1,9 @@
+from random import randint
+
+def Match(HomeTeam, AwayTeam):
+
+val = randint(0, 10)  # Integer from 0 to 10, endpoints included
+if val == 0:8:
+	score = randint(0, 4)
+else:
+	score = randint(4,8)
