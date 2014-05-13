b1f75ea317eade0abea32395c26a007069027f0c
magicdoughnut <magicdoughnut@github.com>
2014-05-13 18:51:31 +0100

    MyLeague.py changed to World Cup

--- MyLeague.py
+++ MyLeague.py
@@ -6,18 +6,18 @@ from TeamClass   import Team
 #from Match import Score
 
 # Let's set up a really simple league!
-MyLeague = League('The Cornish First Division', 2)
+MyLeague = League('The World Cup Group Stages', 2)
 
 # Add a couple of silly teams
-MyLeague.AddTeam('Mark F.C.')
-MyLeague.AddTeam('Jack Rovers')
-MyLeague.AddTeam('Bastard United')
+MyLeague.AddTeam('Uruguay')
+MyLeague.AddTeam('England')
+MyLeague.AddTeam('Italy')
 
 # Print out all the teams in our league
 MyLeague.PrintTeams()
 
 # Now lets say 'Bastard United' get relegated
-MyLeague.RemoveTeam('Bastard United')
+MyLeague.RemoveTeam('England')
 
 # Print out all the teams in our league again
 MyLeague.PrintTeams()
